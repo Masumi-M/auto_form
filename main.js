afe11ede7e8a2d2a328055c5e6b3f3632657988e
Masumi Morishige <masumi_morishige@Lunch-Box.local>
2020-04-14 04:12:31 +0900

    Fixed: main.js

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const puppeteer = require('puppeteer');
 const input_form = {
     name: ['entry.1260234182','Masumi'],
     free: ['entry.149339606', 'Hello World.']
-}
+};
 
 (async () =>{
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://forms.gle/hHGxSqDqRKuBWCcd6');
     await page.type(`input[name="${input_form.name[0]}"]`, input_form.name[1]);
-    await page.type(`input[name="${input_form.free[0]}"]`, input_form.free[1]);
-    
-})
+    await page.type(`textarea[name="${input_form.free[0]}"]`, input_form.free[1]);
+    await page.screenshot({path: 'screenshot.png'});
+    await browser.close();
+})();
